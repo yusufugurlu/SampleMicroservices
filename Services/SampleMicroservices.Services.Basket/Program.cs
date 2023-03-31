a1cd1723f0ac0c7141ec533d7986315d9e9a3cb4
yusufugurlu <yusuf_ugurlu2015@outlook.com>
2023-03-31 00:07:11 -0700

    added  options pattern

---
+++ Services/SampleMicroservices.Services.Basket/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.DependencyInjection;
+using SampleMicroservices.Services.Basket.Settings;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings")); // options pattern
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Services/SampleMicroservices.Services.Basket/Settings/RedisSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace SampleMicroservices.Services.Basket.Settings
+{
+    public class RedisSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
