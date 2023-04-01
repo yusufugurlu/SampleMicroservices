035688198daff0a784978fccaa06762cbeccd1fe
yusufugurlu <yusuf_ugurlu2015@outlook.com>
2023-04-01 16:30:25 -0700

    add policy

--- Services/SampleMicroservices.Services.Basket/Program.cs
+++ Services/SampleMicroservices.Services.Basket/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using SampleMicroservices.Services.Basket.Services;
@@ -8,14 +11,32 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+var requiredAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build(); // need to be Authorization   Authorization olmus biri olmasi lazim
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["identityServerUrl"];
+    options.Audience = "resource_catalog";
+    options.RequireHttpsMetadata = false;
+
+});
+
+
+builder.Services.AddControllers(opt => {
+    opt.Filters.Add(new  AuthorizeFilter(requiredAuthorizePolicy));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 builder.Services.AddHttpContextAccessor();
 builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings")); // options pattern
 builder.Services.AddScoped<ISharedIdentityService,SharedIdentityService>();
+builder.Services.AddScoped<IBasketService, BasketService>();
+
 
 builder.Services.AddSingleton<RedisService>(sp =>
 {
@@ -37,6 +58,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
